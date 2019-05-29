6a38b86cad2467196cee5f24e6f0884343836bb8
KaMa75 <karolmarszalek@o2.pl>
2019-05-29 17:37:57 +0200

    new module and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarsListComponent } from './cars/cars-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsListComponent
   ],
   imports: [
     BrowserModule
