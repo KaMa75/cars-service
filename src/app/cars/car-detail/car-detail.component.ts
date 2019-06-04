137ef5a2cb843546f8b8c4a92ee5f7da17a0f7b0
KaMa75 <karolmarszalek@o2.pl>
2019-06-04 14:31:51 +0200

    add update data

--- src/app/cars/car-detail/car-detail.component.ts
+++ src/app/cars/car-detail/car-detail.component.ts
@@ -1,7 +1,8 @@
 import { Car } from './../models/car';
 import { CarsService } from './../cars.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-car-detail',
@@ -12,10 +13,30 @@ export class CarDetailComponent implements OnInit {
 
   car: Car;
 
-  constructor(private carsService: CarsService, private route: ActivatedRoute) { }
+  carForm: FormGroup;
+
+  constructor(private carsService: CarsService, private route: ActivatedRoute, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.loadCar();
+    this.carForm = this.buildCarForm();
+  }
+
+  buildCarForm() {
+    return this.formBuilder.group({
+      model: [this.car.model , Validators.required],
+      type: [this.car.type],
+      plate: [this.car.plate, [Validators.required, Validators.minLength(3), Validators.maxLength(7)]],
+      deliveryDate: [this.car.deliveryDate],
+      deadline: [this.car.deadline],
+      color: [this.car.color],
+      power: [this.car.power],
+      clientFirstName: [this.car.clientFirstName],
+      clientSurname: [this.car.clientSurname],
+      cost: [this.car.cost],
+      isFullyDamaged: [this.car.isFullyDamaged],
+      year: [this.car.year]
+    });
   }
 
   loadCar() {
@@ -24,6 +45,12 @@ export class CarDetailComponent implements OnInit {
     //   this.car = car;
     // });
     this.car = this.route.snapshot.data['car'];
+  }
+
+  updateCar() {
+    this.carsService.updateCar(this.car.id, this.carForm.value).subscribe(() => {
+      this.router.navigate(['/cars']);
+    });
   }
 
 }
